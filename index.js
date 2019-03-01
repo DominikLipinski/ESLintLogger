7dcdf6e3c70d5285d47556763402973d06caab2b
DominikLipinski <maboonga@gmail.com>
2019-03-01 18:40:37 +0100

    updated eslintrc.json and code

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-'use strict'; 
-var chalk = require( 'chalk' )
-	,Logger = {
-		success:function(msg) {
-			console.log(chalk.green.bold('OK: '), msg);
+/* eslint no-console: 0 */
+'use strict';
+const chalk = require( 'chalk' ),
+	Logger = {
+		success: ( msg ) => {
+			console.log( chalk.green.bold( 'OK: ' ), msg );
 		},
-		error:function(msg) {
-			console.log(chalk.red.bold('Error: '), msg);
+		error: ( msg ) => {
+			console.log( chalk.red.bold( 'Error: ' ), msg );
 		},
-		warning:function(msg) {
-			console.log(chalk.yellow.bold('Warning: '), msg);
+		warning: ( msg ) => {
+			console.log( chalk.yellow.bold( 'Warning: ' ), msg );
 		},
-		info:function(msg) {
-			console.log(chalk.blue.bold('Info:'), msg);
+		info: ( msg ) => {
+			console.log( chalk.blue.bold( 'Info:' ), msg );
 		}
-	}; 
+	};
 
-Logger.success('Logger working!');
-Logger.error('Logger NOT working!');
-Logger.warning('Logger has problem!');
-Logger.info('Logger OK!');
+Logger.success( 'Logger working!' );
+Logger.error( 'Logger NOT working!' );
+Logger.warning( 'Logger has problem!' );
+Logger.info( 'Logger OK!' );
